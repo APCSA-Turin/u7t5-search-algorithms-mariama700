f11d938973881680f37c103a46ed1d4fd7915eb6
mariam <mariama70@nycstudents.net>
2025-02-18 04:09:19 +0000

    Gradle tests pass

--- src/main/java/com/example/project/Repair/RepairSchedule.java
+++ src/main/java/com/example/project/Repair/RepairSchedule.java
@@ -12,7 +12,8 @@ public class RepairSchedule {
      * Precondition: n >= 0
      */
     public RepairSchedule(int n) {
-        /* to be implemented in part (a) */
+        this.numberOfMechanics = n;
+        this.schedule = new ArrayList<>();
     }
 
     public ArrayList<CarRepair> getSchedule() {
@@ -23,6 +24,12 @@ public class RepairSchedule {
      * Precondition: 0 <= m < numberOfMechanics and b >= 0
      */
     public boolean addRepair(int m, int b) {
+        for (CarRepair repair : schedule) {
+            if (repair.getMechanicNum() == m || repair.getBayNum() == b) {
+                return false;
+            }
+        }
+        schedule.add(new CarRepair(m, b));
         return true;
     }
 
@@ -30,7 +37,18 @@ public class RepairSchedule {
      * as described in part (b).
      */
     public ArrayList<Integer> availableMechanics() {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> available = new ArrayList<>();
+        ArrayList<Integer> assigned = new ArrayList<>();
+        for (CarRepair repair : schedule) {
+            assigned.add(repair.getMechanicNum());
+        }
+        for (int i = 0; i < numberOfMechanics; i++) {
+            if (!assigned.contains(i)) {
+                available.add(i);
+            }
+        }
+
+        return available;
     }
 
     /** Removes an element from schedule when a repair is complete.
